87305c12699d937a8395a43fe41a4bb458e0a507
mgeipel <m.geipel@dnb.de>
2013-03-06 13:15:32 +0100

    added bash script for visualizing Metamorph definitions.

---
+++ src/main/scripts/visualizeMorphDefs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+  echo "Usage: `basename $0` OUTPUT_FORMAT DOT_FILES+"
+  exit 65
+fi
+
+
+FORMAT=$1
+shift
+for FILE in $@
+do
+    echo visualizing $FILE
+    java org.culturegraph.mf.MorphVis $FILE | dot -T$FORMAT > $FILE.$FORMAT
+done
