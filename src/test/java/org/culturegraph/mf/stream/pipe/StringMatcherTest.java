e1faaad06e689de67327ebba267abca516a55cba
Christoph Böhme <christoph@b3e.net>
2013-03-16 21:52:53 +0100

    Changed StringMatcherTest to use mocks

--- src/test/java/org/culturegraph/mf/stream/pipe/StringMatcherTest.java
+++ src/test/java/org/culturegraph/mf/stream/pipe/StringMatcherTest.java
@@ -15,8 +15,14 @@
  */
 package org.culturegraph.mf.stream.pipe;
 
-import org.junit.Assert;
+import static org.mockito.Mockito.verify;
+
+import org.culturegraph.mf.framework.ObjectReceiver;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 
 /**
@@ -25,26 +31,54 @@ import org.junit.Test;
  */
 public final class StringMatcherTest {
 
+	private static final String INPUT_STRING = 
+			"The pattern is not part of the input";
+	
+	private StringMatcher matcher;
+	
+	@Mock
+	private ObjectReceiver<String> receiver;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		
+		matcher = new StringMatcher();
+		matcher.setReceiver(receiver);
+	}
+	
+	@After
+	public void cleanup() {
+		matcher.closeStream();
+	}
+	
 	@Test
-	public void testMatcher() {
-		final StringMatcher matcher = new StringMatcher();
-		final ObjectBuffer<String> result = new ObjectBuffer<String>();
-		
-		matcher.setReceiver(result);
-		
+	public void testShouldReplaceAllMatchesWithReplacementString() {
 		matcher.setPattern("PLACEHOLDER");
 		matcher.setReplacement("Karl");
-		matcher.process("Hi PLACEHOLDER! -- Bye PLACEHOLDER!");
 		
+		matcher.process("Hi PLACEHOLDER! -- Goodbye PLACEHOLDER!");
+		
+		verify(receiver).process("Hi Karl! -- Goodbye Karl!");
+	}
+	
+	@Test
+	public void testShouldHandleCaptureGroupReferencesInReplacementString() {
 		matcher.setPattern("^([^ ]+) .*$");
 		matcher.setReplacement("$1");
-		matcher.process("important-bit this can be ignored");
 		
-		matcher.closeStream();
+		matcher.process("important-bit but this can be ignored");
 		
-		Assert.assertEquals("Hi Karl! -- Bye Karl!", result.pop());
-		Assert.assertEquals("important-bit", result.pop());
-		Assert.assertNull(result.pop());
+		verify(receiver).process("important-bit");
 	}
-
+	
+	@Test
+	public void testShouldRelayNonMatchingInput() {
+		matcher.setPattern("Non-matching pattern");
+		
+		matcher.process(INPUT_STRING);
+		
+		verify(receiver).process(INPUT_STRING);
+	}
+	
 }
