5df3ab70cd3fdb7cc0733891da36d7af521ac1b7
Christoph Böhme <c.boehme@dnb.de>
2013-05-16 11:34:00 +0200

    Fixed #82
    Created a utility class for time related helper functions and moved non
    time logging specific code from TimerBase. Added test cases.

--- src/main/java/org/culturegraph/mf/stream/pipe/TimerBase.java
+++ src/main/java/org/culturegraph/mf/stream/pipe/TimerBase.java
@@ -17,22 +17,20 @@ package org.culturegraph.mf.stream.pipe;
 
 import org.culturegraph.mf.framework.LifeCycle;
 import org.culturegraph.mf.framework.Sender;
+import org.culturegraph.mf.util.TimeUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 /**
  * @author Christoph Böhme
- * 
+ *
  * @param <R>
  *            receiver type.
  */
 public class TimerBase<R extends LifeCycle> implements Sender<R> {
 
 	private static final Logger LOG = LoggerFactory.getLogger(TimerBase.class);
-
-	private static final String[] UNIT_NAMES = { "ns", "µs", "ms", "s", "min", "h" };
-	private static final long[] UNIT_FACTORS = { 1, 1000, 1000, 1000, 60, 60 };
 
 	private final String logPrefix;
 
@@ -47,7 +45,7 @@ public class TimerBase<R extends LifeCycle> implements Sender<R> {
 		this.receiver = receiver;
 		return receiver;
 	}
-	
+
 	public R getReceiver() {
 		return receiver;
 	}
@@ -66,13 +64,13 @@ public class TimerBase<R extends LifeCycle> implements Sender<R> {
 		final long averageDuration = cumulativeDuration / count;
 		LOG.info(logPrefix
 				+ String.format("Executions: %d; Cumulative duration: %s; Average duration: %s", Long.valueOf(count),
-						scaleTime(cumulativeDuration), scaleTime(averageDuration)));
+						TimeUtil.formatDuration(cumulativeDuration), TimeUtil.formatDuration(averageDuration)));
 		startMeasurement();
 		if (receiver != null) {
 			receiver.closeStream();
 		}
 		stopMeasurement("Time to close stream: ");
-				
+
 	}
 
 	protected TimerBase(final String logPrefix) {
@@ -83,7 +81,7 @@ public class TimerBase<R extends LifeCycle> implements Sender<R> {
 	protected final void startMeasurement() {
 		startTime = System.nanoTime();
 	}
-	
+
 	protected final void stopMeasurement(){
 		stopMeasurement("Execution %1$d:");
 	}
@@ -94,23 +92,7 @@ public class TimerBase<R extends LifeCycle> implements Sender<R> {
 		count += 1;
 		cumulativeDuration += duration;
 
-		LOG.info(logPrefix + String.format(prefix + " %2$s", Long.valueOf(count), scaleTime(duration)));
-	}
-
-	private static String scaleTime(final long time) {
-		long major = time;
-		long minor;
-		int i = -1;
-		do {
-			i += 1;
-			minor = major % UNIT_FACTORS[i];
-			major /= UNIT_FACTORS[i];
-		} while (i == UNIT_FACTORS.length || major >= UNIT_FACTORS[i + 1]);
-
-		if (i == 0) {
-			return String.format("%d%s", Long.valueOf(major), UNIT_NAMES[0]);
-		}
-		return String.format("%d%s %d%s", Long.valueOf(major), UNIT_NAMES[i], Long.valueOf(minor), UNIT_NAMES[i - 1]);
+		LOG.info(logPrefix + String.format(prefix + " %2$s", Long.valueOf(count), TimeUtil.formatDuration(duration)));
 	}
 
 }
