0cbf6eb7c45db3105ba0d630962d384a3ddc2819
Christoph Böhme <christoph@b3e.net>
2014-02-28 22:27:15 +0100

    Fix issue #60: Place OreAggregationAdder and its test in same package
    The OreAggregationAdder and its corresponding test cases were in
    different packages. This commit moves the test case into the same
    package as the main class.

--- src/test/java/org/culturegraph/mf/stream/pipe/OreAggregationAdderTest.java
+++ src/test/java/org/culturegraph/mf/stream/pipe/OreAggregationAdderTest.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.culturegraph.mf.stream.pipe.bib;
+package org.culturegraph.mf.stream.pipe;
 
 import org.culturegraph.mf.stream.pipe.OreAggregationAdder;
 import org.culturegraph.mf.test.TestSuite;
