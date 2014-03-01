2b7bcc20876c73d79e65764aa3299a7b84879f78
Christoph Böhme <christoph@b3e.net>
2014-03-01 17:03:46 +0100

    Updated date in copyright notice and added license file

--- src/main/java/org/culturegraph/mf/stream/converter/bib/PicaParserState.java
+++ src/main/java/org/culturegraph/mf/stream/converter/bib/PicaParserState.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2013 Christoph Böhme
+ *  Copyright 2013, 2014 Christoph Böhme
  *
  *  Licensed under the Apache License, Version 2.0 the "License";
  *  you may not use this file except in compliance with the License.
--- src/main/java/org/culturegraph/mf/stream/pipe/TimerBase.java
+++ src/main/java/org/culturegraph/mf/stream/pipe/TimerBase.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2013 Deutsche Nationalbibliothek
+ *  Copyright 2013, 2014 Deutsche Nationalbibliothek
  *
  *  Licensed under the Apache License, Version 2.0 the "License";
  *  you may not use this file except in compliance with the License.
