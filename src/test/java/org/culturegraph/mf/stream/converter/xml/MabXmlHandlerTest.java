c206f5fcf40e22471c5c440ccfd7ba1e9c3ce275
pascal Christoph <christoph@hbz-nrw.de>
2014-05-26 15:47:08 +0200

    Add Mab Xml handler
    * add a usage example as flux test

---
+++ src/test/java/org/culturegraph/mf/stream/converter/xml/MabXmlHandlerTest.java
@@ -0,0 +1,24 @@
+/* Copyright 2013  hbz, Pascal Christoph.
+ * Licensed under the Eclipse Public License 1.0 */
+package org.culturegraph.mf.stream.converter.xml;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.antlr.runtime.RecognitionException;
+import org.culturegraph.mf.Flux;
+import org.junit.Test;
+
+/**
+ * @author Pascal Christoph (dr0i)
+ *
+ */
+public final class MabXmlHandlerTest {
+
+	@Test
+	public void testFlux() throws URISyntaxException, IOException, RecognitionException {
+		final File fluxFile = new File("examples/read/mab2/mabXml.flux");
+		Flux.main(new String[] { fluxFile.getAbsolutePath() });
+	}
+}
