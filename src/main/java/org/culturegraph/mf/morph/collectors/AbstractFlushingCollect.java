f4f7c28de01826ed5b563413d174b5925a71d06a
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-07-22 15:16:47 +0200

    made update hierarchical entity optional in flush method of collector

--- src/main/java/org/culturegraph/mf/morph/collectors/AbstractFlushingCollect.java
+++ src/main/java/org/culturegraph/mf/morph/collectors/AbstractFlushingCollect.java
@@ -40,7 +40,10 @@ public abstract class AbstractFlushingCollect extends AbstractCollect {
 			}
 		}
 
-		updateHierarchicalEntity(entityCount);
+		if(getIncludeSubEntities()) {
+
+			updateHierarchicalEntity(entityCount);
+		}
 	}
 
 }
