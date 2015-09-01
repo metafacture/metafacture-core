51c101525fb2bf3c346821e37a90be13d9862d2b
Thomas Gängler <tgaengler@avantgarde-labs.de>
2015-09-01 10:57:43 +0200

    [DD-1236] fixed dewey processing bug (incl. tests)

--- src/test/java/org/culturegraph/mf/morph/functions/DeweyTest2.java
+++ src/test/java/org/culturegraph/mf/morph/functions/DeweyTest2.java
@@ -7,5 +7,5 @@ import org.junit.runner.RunWith;
  * @author tgaengler
  */
 @RunWith(TestSuite.class)
-@TestSuite.TestDefinitions({"Dewey.xml", "Dewey2.xml", "Dewey3.xml", "Dewey4.xml"})
+@TestSuite.TestDefinitions({"Dewey.xml", "Dewey2.xml", "Dewey3.xml", "Dewey4.xml", "Dewey5.xml", "Dewey31.xml", "Dewey41.xml"})
 public final class DeweyTest2 {/*bind to xml test*/}
