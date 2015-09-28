e893656af37a418dac9b5b8b65f1f4f25717bfd8
Thomas Gängler <tgaengler@avantgarde-labs.de>
2015-09-28 16:19:29 +0200

    [DD-584] added first draft of ISSN function (incl. tests)

---
+++ src/test/java/org/culturegraph/mf/morph/functions/ISSNTest2.java
@@ -0,0 +1,13 @@
+package org.culturegraph.mf.morph.functions;
+
+import org.culturegraph.mf.test.TestSuite;
+import org.junit.runner.RunWith;
+
+/**
+ * @author tgaengler
+ */
+@RunWith(TestSuite.class)
+@TestSuite.TestDefinitions({"ISSN.xml", "ISSN2.xml", "ISSN3.xml", "ISSN4.xml"})
+public class ISSNTest2 {
+
+}
