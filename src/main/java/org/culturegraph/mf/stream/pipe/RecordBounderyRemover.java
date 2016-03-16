c337ce973e66181c282dd6904f828518ce874ae3
Christoph Böhme <christoph@b3e.net>
2016-03-16 15:35:48 +0100

    Add module to turn records into entities
    The `RecordToEntity` module replaces start-record and end-record events
    with start- and end-entity events. This functionality is similar to the
    behaviour of the `RecordBoundaryRemover` when created with an entity
    name. However, the new module is more flexible as the entity name is
    specified using a setter and the output of the id literal is also
    configurable. Therefore, the single argument constructor in
    `RecordBoundaryRemover` is marked as deprecated with the intention of
    removing it in a future release.

--- src/main/java/org/culturegraph/mf/stream/pipe/RecordBounderyRemover.java
+++ src/main/java/org/culturegraph/mf/stream/pipe/RecordBounderyRemover.java
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2013, 2014 Deutsche Nationalbibliothek
+ * Copyright 2016 Deutsche Nationalbibliothek
  *
- *  Licensed under the Apache License, Version 2.0 the "License";
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.culturegraph.mf.stream.pipe;
 
@@ -25,29 +25,38 @@ import org.culturegraph.mf.util.StreamConstants;
 
 /**
  * Removes record boundaries
- * 
+ *
  * @author Markus Michael Geipel
  *
  */
 @Description("Removes record boundaries")
 @In(StreamReceiver.class)
 @Out(StreamReceiver.class)
-public final class RecordBounderyRemover 
+public final class RecordBounderyRemover
 		extends DefaultStreamPipe<StreamReceiver> {
-	
+
 	private final String entityName;
-	
+
+	/**
+	 * Creates an instance of RecordBounderyRemover that replaces the record
+	 * events with entity events.
+	 *
+	 * @param entityName name of the generated entity.
+	 * @deprecated Use {@link RecordToEntity} instead. This constructor will be
+	 * removed in release 4.0.0.
+	 */
+	@Deprecated
 	public RecordBounderyRemover(final String entityName) {
 		super();
 		this.entityName = entityName;
 	}
-	
+
 	public RecordBounderyRemover(){
 		super();
 		this.entityName = null;
-		
+
 	}
-	
+
 	@Override
 	public void startRecord(final String identifier) {
 		assert !isClosed();
@@ -84,4 +93,5 @@ public final class RecordBounderyRemover
 		assert !isClosed();
 		getReceiver().literal(name, value);
 	}
+
 }
