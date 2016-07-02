aa5cb7e53325848620721fb42995434f1eba9dea
Christoph Böhme <christoph@b3e.net>
2016-07-02 22:27:00 +0200

    Refactor test cases for StreamTee
    Removes the dependencies on `EventList` and `StreamValidator`.

--- src/test/java/org/culturegraph/mf/stream/pipe/StreamTeeTest.java
+++ src/test/java/org/culturegraph/mf/stream/pipe/StreamTeeTest.java
@@ -15,53 +15,67 @@
  */
 package org.culturegraph.mf.stream.pipe;
 
-import static org.junit.Assert.fail;
+import static org.mockito.Mockito.inOrder;
 
-import org.culturegraph.mf.exceptions.FormatException;
 import org.culturegraph.mf.framework.StreamReceiver;
-import org.culturegraph.mf.stream.sink.EventList;
-import org.culturegraph.mf.stream.sink.StreamValidator;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 
 /**
  * Tests for class {@link StreamTee}.
  *
  * @author Markus Michael Geipel
+ * @author Christoph Böhme (refactored to Mockito)
  *
  */
 public final class StreamTeeTest {
 
-	@Test
-	public void testCorrectTeeFunction() {
-		final EventList list = new EventList();
-		execTestEvents(list);
+	@Mock
+	private StreamReceiver receiver1;
 
-		final StreamValidator validator1 = new StreamValidator(list.getEvents());
-		final StreamValidator validator2 = new StreamValidator(list.getEvents());
-		final StreamTee tee = new StreamTee();
-		tee.addReceiver(validator1)
-			.addReceiver(validator2);
+	@Mock
+	private StreamReceiver receiver2;
 
-		try {
-			execTestEvents(tee);
-			tee.closeStream();
-		} catch (FormatException e) {
-			fail("Tee did not forward data as expected: " + e);
-		}
+	private StreamTee streamTee;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		streamTee = new StreamTee();
+		streamTee
+				.addReceiver(receiver1)
+				.addReceiver(receiver2);
 	}
 
-	private void execTestEvents(final StreamReceiver receiver) {
-		receiver.startRecord("1");
-		receiver.literal("l1", "value1");
-		receiver.literal("l1", "value2");
-		receiver.startEntity("e1");
-		receiver.literal("l2", "value3");
-		receiver.endEntity();
-		receiver.endRecord();
-		receiver.startRecord("2");
-		receiver.literal("l3", "value4");
-		receiver.endRecord();
+	@Test
+	public void shouldForwardAllEventsToAllDownStreamReceivers() {
+		streamTee.startRecord("1");
+		streamTee.literal("literal", "value");
+		streamTee.startEntity("entity");
+		streamTee.endEntity();
+		streamTee.endRecord();
+		streamTee.resetStream();
+		streamTee.closeStream();
+
+		final InOrder ordered = inOrder(receiver1, receiver2);
+		ordered.verify(receiver1).startRecord("1");
+		ordered.verify(receiver2).startRecord("1");
+		ordered.verify(receiver1).literal("literal", "value");
+		ordered.verify(receiver2).literal("literal", "value");
+		ordered.verify(receiver1).startEntity("entity");
+		ordered.verify(receiver2).startEntity("entity");
+		ordered.verify(receiver1).endEntity();
+		ordered.verify(receiver2).endEntity();
+		ordered.verify(receiver1).endRecord();
+		ordered.verify(receiver2).endRecord();
+		ordered.verify(receiver1).resetStream();
+		ordered.verify(receiver2).resetStream();
+		ordered.verify(receiver1).closeStream();
+		ordered.verify(receiver2).closeStream();
 	}
 
 }
