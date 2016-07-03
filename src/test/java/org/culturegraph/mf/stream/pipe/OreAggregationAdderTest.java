febd65c59adbccc38cbaabce2e9c1c544046a188
Christoph Böhme <christoph@b3e.net>
2016-07-03 16:01:33 +0200

    Refactor test cases for OreAggregationAdder
    Replaces the Metamorph Test based test with a Mockito based one.

--- src/test/java/org/culturegraph/mf/stream/pipe/OreAggregationAdderTest.java
+++ src/test/java/org/culturegraph/mf/stream/pipe/OreAggregationAdderTest.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2016 Christoph Böhme
  * Copyright 2013, 2014 Deutsche Nationalbibliothek
  *
  * Licensed under the Apache License, Version 2.0 the "License";
@@ -15,15 +16,62 @@
  */
 package org.culturegraph.mf.stream.pipe;
 
-import org.culturegraph.mf.test.MetamorphTestSuite;
-import org.junit.runner.RunWith;
+import static org.mockito.Mockito.inOrder;
+
+import org.culturegraph.mf.framework.StreamReceiver;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 
 /**
  * Tests for class {@link OreAggregationAdder}
  *
  * @author Markus Michael Geipel
+ * @author Christoph Böhme (refactored to Mockito)
  *
  */
-@RunWith(MetamorphTestSuite.class)
-public final class OreAggregationAdderTest {/*bind to xml test*/}
+public final class OreAggregationAdderTest {
+
+	@Mock
+	private StreamReceiver receiver;
+
+	private OreAggregationAdder oreAggregationAdder;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		oreAggregationAdder = new OreAggregationAdder();
+		oreAggregationAdder.setReceiver(receiver);
+	}
+
+	@Test
+	public void shouldAddWebResource() {
+		oreAggregationAdder.startRecord("1");
+		oreAggregationAdder.literal("copyright", "CC-0");
+		oreAggregationAdder.literal("aggregation_id", "hawaii_aggregation");
+		oreAggregationAdder.startEntity("edm:WebResource");
+		oreAggregationAdder.literal("~rdf:about", "hawaii");
+		oreAggregationAdder.literal("hula", "hula");
+		oreAggregationAdder.endEntity();
+		oreAggregationAdder.endRecord();
+
+		final InOrder ordered = inOrder(receiver);
+		ordered.verify(receiver).startRecord("1");
+		ordered.verify(receiver).startEntity("edm:WebResource");
+		ordered.verify(receiver).literal("~rdf:about", "hawaii");
+		ordered.verify(receiver).literal("hula", "hula");
+		ordered.verify(receiver).endEntity();
+		ordered.verify(receiver).startEntity("ore:Aggregation");
+		ordered.verify(receiver).literal("~rdf:about", "hawaii_aggregation");
+		ordered.verify(receiver).startEntity("edm:isShownBy");
+		ordered.verify(receiver).literal("~rdf:resource", "hawaii");
+		ordered.verify(receiver).endEntity();
+		ordered.verify(receiver).literal("copyright", "CC-0");
+		ordered.verify(receiver).endEntity();
+		ordered.verify(receiver).endRecord();
+	}
+
+}
