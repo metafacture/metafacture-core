975eada0247cf00a57baf3f351eab2846d83ed0c
Christoph Böhme <christoph@b3e.net>
2016-10-28 07:27:18 +0200

    AseqDecoder: remove unused code and refactorings
    The commit removes unused variables and constants. The static `process`
    methods are removed. Those were a relict from the beginnings of
    Metafacture which should no longer be used.

--- src/main/java/org/culturegraph/mf/stream/converter/bib/AseqDecoder.java
+++ src/main/java/org/culturegraph/mf/stream/converter/bib/AseqDecoder.java
@@ -15,8 +15,6 @@
  */
 package org.culturegraph.mf.stream.converter.bib;
 
-import java.util.regex.Pattern;
-
 import org.culturegraph.mf.framework.DefaultObjectPipe;
 import org.culturegraph.mf.framework.StreamReceiver;
 import org.culturegraph.mf.framework.annotations.Description;
@@ -24,86 +22,47 @@ import org.culturegraph.mf.framework.annotations.In;
 import org.culturegraph.mf.framework.annotations.Out;
 
 /**
- * Parses a raw Aseq stream. UTF-8 encoding expected. Events are handled by a
- * {@link StreamReceiver}.
- *
- * @see StreamReceiver
+ * Parses a raw Aseq stream. UTF-8 encoding expected.
  *
  * @author Lars G. Svensson
  */
 @Description("Parses a raw Aseq record (UTF-8 encoding expected).")
 @In(String.class)
 @Out(StreamReceiver.class)
-public final class AseqDecoder extends
-		DefaultObjectPipe<String, StreamReceiver> {
+public final class AseqDecoder
+		extends DefaultObjectPipe<String, StreamReceiver> {
 
 	private static final String FIELD_DELIMITER = "\n";
-	private static final String SUB_DELIMITER = "\u001f";
-	private static final Pattern FIELD_PATTERN = Pattern.compile(
-			FIELD_DELIMITER, Pattern.LITERAL);
-	private static final Pattern SUBFIELD_PATTERN = Pattern.compile(
-			SUB_DELIMITER, Pattern.LITERAL);
-	private static final int POS_ENCODING = 9;
-	private static final int POS_DIRECTORY = 24;
-	private static final int DIRECTORY_ENTRY_WIDTH = 12;
-	private static final int TAG_LENGTH = 3;
-	private static final int DATA_START_BEGIN = 12;
-	private static final int DATA_START_END = 17;
-
-	private static final Object ID_TAG = "001";
-	private static final String LEADER = "leader";
-	private static final int LEADER_END = 24;
-
-	private boolean checkUtf8Encoding;
 
 	@Override
 	public void process(final String record) {
 		assert !isClosed();
-		process(record, getReceiver(), this.checkUtf8Encoding);
-	}
-
-	public void setCheckUtf8Encoding(final boolean checkUtf8Encoding) {
-		this.checkUtf8Encoding = checkUtf8Encoding;
-	}
-
-	public static void process(final String record,
-			final StreamReceiver receiver) {
-		process(record, receiver, false);
-	}
-
-	public static void process(String record, final StreamReceiver receiver,
-			final boolean checkUtf8encoding) {
-		record = record.trim();
-		if (record.isEmpty()) {
+		final String trimedRecord = record.trim();
+		if (trimedRecord.isEmpty()) {
 			return;
 		}
-		final String[] lines = record.split(FIELD_DELIMITER);
+		final String[] lines = trimedRecord.split(FIELD_DELIMITER);
 		for (int i = 0; i < lines.length; i++) {
 			final String field = lines[i];
 			if (i == 0) {
-				receiver.startRecord(field.substring(0, 9));
+				getReceiver().startRecord(field.substring(0, 9));
 			}
 			final String category = field.substring(10, 15).trim();
-			final String scriptCode = field.substring(16, 17);
 			final String fieldContent = field.substring(18).trim();
 			if (!fieldContent.startsWith("$$")) {
-				receiver.literal(category, fieldContent);
+				getReceiver().literal(category, fieldContent);
 			} else {
-				receiver.startEntity(category);
+				getReceiver().startEntity(category);
 				final String[] subfields = fieldContent.split("\\$\\$");
-				for (int j = 0; j < subfields.length; j++) {
-					if (!"".equals(subfields[j])) {
-						final String subfield = subfields[j];
-						final String subfieldCode = subfield.substring(0, 1);
-						final String subfieldContent = subfield.substring(1);
-						receiver.literal(subfield.substring(0, 1),
-								subfield.substring(1));
+				for (final String subfield : subfields) {
+					if (!subfield.isEmpty()) {
+						getReceiver().literal(subfield.substring(0, 1), subfield.substring(1));
 					}
 				}
-				receiver.endEntity();
+				getReceiver().endEntity();
 			}
 		}
-		receiver.endRecord();
+		getReceiver().endRecord();
 	}
 
 }
