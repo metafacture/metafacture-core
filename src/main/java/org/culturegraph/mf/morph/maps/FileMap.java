f88f70cf135e60db4ffc16bf082d2b2125bfeb33
Christoph Böhme <christoph@b3e.net>
2016-11-29 17:54:09 +0100

    Metamorph: Rewrite open file logic in FileMap
    `FileMap` used `ResourceUtil#getStream(String)` for opening the map
    data files. This is the only use of a method from the `util` package in
    Metamorph's functions, collectors and maps. By removing this dependency
    it becomes easier to split Metamorph in a base module and a core
    functionality module as planned in the Metafacture 5.0.0 release.
    
    The rewrite duplicates the code in `ResourceUtil#getStream(String)`.
    However, this is only a temporary issue. In a future release the `files`
    attribute will be revised to accept URL patterns as defined by the
    xinclude standard (see issue #261).

--- src/main/java/org/culturegraph/mf/morph/maps/FileMap.java
+++ src/main/java/org/culturegraph/mf/morph/maps/FileMap.java
@@ -16,18 +16,22 @@
 package org.culturegraph.mf.morph.maps;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.regex.Pattern;
 
 import org.culturegraph.mf.morph.api.MorphException;
 import org.culturegraph.mf.morph.api.helpers.AbstractReadOnlyMap;
-import org.culturegraph.mf.util.ResourceUtil;
 
 /**
  * Provides a {@link Map} based on a file. The file is supposed to be UTF-8
@@ -51,9 +55,11 @@ public final class FileMap extends AbstractReadOnlyMap<String, String> {
 	}
 
 	public void setFile(final String file) {
-		try (InputStream stream = ResourceUtil.getStream(file);
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+		try (
+				final InputStream stream = openStream(file);
+				final BufferedReader reader = new BufferedReader(
+						new InputStreamReader(stream, StandardCharsets.UTF_8))
+		) {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				if (line.isEmpty()) {
@@ -64,10 +70,43 @@ public final class FileMap extends AbstractReadOnlyMap<String, String> {
 					map.put(parts[0], parts[1]);
 				}
 			}
-		} catch (final FileNotFoundException e) {
-			throw new MorphException("resource '" + file + "' not found", e);
-		} catch (final IOException e) {
+		} catch (final IOException | UncheckedIOException e) {
 			throw new MorphException(e);
+		}
+	}
+
+	private InputStream openStream(String file) {
+		return openAsFile(file)
+				.orElseGet(() -> openAsResource(file)
+						.orElseGet(() -> openAsUrl(file)
+								.orElseThrow(() -> new MorphException(
+										"File not found: " + file))));
+	}
+
+	private Optional<InputStream> openAsFile(String file) {
+		try {
+			return Optional.of(new FileInputStream(file));
+		} catch (FileNotFoundException e) {
+			return Optional.empty();
+		}
+	}
+
+	private Optional<InputStream> openAsResource(String file) {
+		return Optional.ofNullable(Thread.currentThread()
+				.getContextClassLoader().getResourceAsStream(file));
+	}
+
+	private Optional<InputStream> openAsUrl(String file) {
+		final URL url;
+		try {
+			url = new URL(file);
+		} catch (MalformedURLException e) {
+			return Optional.empty();
+		}
+		try {
+			return Optional.of(url.openStream());
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
 		}
 	}
 
