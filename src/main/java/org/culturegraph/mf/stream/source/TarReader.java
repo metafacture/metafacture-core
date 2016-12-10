98cf9f67ef8c9e16dd58a29e4d935f7803048cf2
Christoph Böhme <christoph@b3e.net>
2016-12-10 17:17:36 +0100

    Refactor TarReader to improve stream handling
    The `try` statements in `TarReader` are replaced with try-with-resource
    statements to ensure we are not leaking any resources.
    
    Individual archive entries are no longer read completely into a string
    before calling the downstream module. Instead of this, the archive
    stream is passed directly to the downstream module. It is only wrapped
    in a small adapter class which works around the special semantics of the
    `close()` method in `ArchiveInputStream`.

--- src/main/java/org/culturegraph/mf/stream/source/TarReader.java
+++ src/main/java/org/culturegraph/mf/stream/source/TarReader.java
@@ -1,4 +1,6 @@
-/* Copyright 2013 hbz, Pascal Christoph.
+/*
+ * Copyright 2013 hbz, Pascal Christoph
+ * Copyright 3016 Christoph Böhme
  *
  *  Licensed under the Apache License, Version 2.0 the "License";
  *  you may not use this file except in compliance with the License.
@@ -12,56 +14,138 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.culturegraph.mf.stream.source;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.StringReader;
+import java.nio.charset.Charset;
 
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.ReaderInputStream;
-import org.culturegraph.mf.framework.helpers.DefaultObjectPipe;
+import org.culturegraph.mf.framework.FluxCommand;
+import org.culturegraph.mf.framework.MetafactureException;
 import org.culturegraph.mf.framework.ObjectReceiver;
 import org.culturegraph.mf.framework.annotations.Description;
-import org.culturegraph.mf.framework.FluxCommand;
 import org.culturegraph.mf.framework.annotations.In;
 import org.culturegraph.mf.framework.annotations.Out;
+import org.culturegraph.mf.framework.helpers.DefaultObjectPipe;
 
 /**
- * Opens (aka 'untar') a tar archive and passes every entry .
+ * Opens (aka 'untar') a tar archive and passes every entry.
  *
  * @author Pascal Christoph (dr0i)
+ * @author Christoph Böhme
  */
 @Description("Opens a tar archive and passes every entry.")
 @In(Reader.class)
 @Out(Reader.class)
 @FluxCommand("open-tar")
-public class TarReader extends DefaultObjectPipe<Reader, ObjectReceiver<Reader>> {
+public class TarReader
+		extends DefaultObjectPipe<Reader, ObjectReceiver<Reader>> {
+
 	@Override
 	public void process(final Reader reader) {
-		TarArchiveInputStream tarInputStream = null;
-		try {
-			tarInputStream = new TarArchiveInputStream(new ReaderInputStream(reader));
-			TarArchiveEntry entry = null;
-			while ((entry = (TarArchiveEntry) tarInputStream.getNextEntry()) != null) {
+		try (
+				InputStream stream = new ReaderInputStream(reader,
+						Charset.defaultCharset());
+				ArchiveInputStream tarStream = new TarArchiveInputStream(stream);
+		) {
+			ArchiveEntry entry;
+			while ((entry = tarStream.getNextEntry()) != null) {
 				if (!entry.isDirectory()) {
-					final byte[] buffer = new byte[(int) entry.getSize()];
-					while ((tarInputStream.read(buffer)) > 0) {
-						getReceiver().process(new StringReader(new String(buffer)));
-					}
+					processFileEntry(tarStream);
 				}
 			}
-			tarInputStream.close();
-		} catch (final FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (final IOException e) {
-			e.printStackTrace();
-		} finally {
-			IOUtils.closeQuietly(tarInputStream);
+		} catch (IOException e) {
+			throw new MetafactureException(e);
 		}
 	}
+
+	private void processFileEntry(ArchiveInputStream archiveStream)
+			throws IOException {
+		try (
+				InputStream entryStream = new ArchiveEntryInputStream(archiveStream);
+				Reader entryReader = new InputStreamReader(entryStream);
+		) {
+			getReceiver().process(entryReader);
+		}
+	}
+
+	/**
+	 * Wraps an {@link ArchiveInputStream} so that individual archive entries
+	 * can be read by code working on arbitrary {@link InputStream}s.
+	 * <p>
+	 * This is required as the semantics of {@code ArchiveInputStream} differ
+	 * from standard {@code InputStream} behaviour. Archive streams must only be
+	 * closed after reading all entries and not after reading a single entry.
+	 * Because of this, it is not possible to pass an {@code ArchiveInputStream}
+	 * to code expecting an {@code InputStream} as usually the received stream
+	 * will be closed after the code is finished with it. In case of an {@code
+	 * ArchiveInputStream} this would not close the stream for the current entry
+	 * but the complete archive stream which might not be expected by the caller .
+	 * <p>
+	 * This class hides this difference by ignoring calls of the
+	 * {@link #close()} method.
+	 *
+	 * @author Christoph Böhme
+	 */
+	private static class ArchiveEntryInputStream extends InputStream {
+
+		private final ArchiveInputStream archiveStream;
+
+		ArchiveEntryInputStream(ArchiveInputStream archiveStream) {
+			this.archiveStream = archiveStream;
+		}
+
+		@Override
+		public int read() throws IOException {
+			return archiveStream.read();
+		}
+
+		@Override
+		public int read(byte[] b) throws IOException {
+			return archiveStream.read(b);
+		}
+
+		@Override
+		public int read(byte[] b, int off, int len) throws IOException {
+			return archiveStream.read(b, off, len);
+		}
+
+		@Override
+		public long skip(long n) throws IOException {
+			return archiveStream.skip(n);
+		}
+
+		@Override
+		public int available() throws IOException {
+			return archiveStream.available();
+		}
+
+		@Override
+		public void close() throws IOException {
+			// Do not delegate calls of close() to the archive stream
+		}
+
+		@Override
+		public void mark(int readlimit) {
+			archiveStream.mark(readlimit);
+		}
+
+		@Override
+		public void reset() throws IOException {
+			archiveStream.reset();
+		}
+
+		@Override
+		public boolean markSupported() {
+			return archiveStream.markSupported();
+		}
+
+	}
+
 }
