5308ef404c9dc5938611b881c839de290d96855a
Christoph Böhme <christoph@b3e.net>
2016-12-22 22:42:42 +0100

    Create statistics package
    The `statistics` package contains modules for computing various
    statistics.

--- src/main/java/org/culturegraph/mf/statistics/Histogram.java
+++ src/main/java/org/culturegraph/mf/statistics/Histogram.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.culturegraph.mf.stream.sink;
+package org.culturegraph.mf.statistics;
 
 import java.util.Collections;
 import java.util.HashMap;
--- src/test/java/org/culturegraph/mf/statistics/CooccurrenceMetricCalculatorTest.java
+++ src/test/java/org/culturegraph/mf/statistics/CooccurrenceMetricCalculatorTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.culturegraph.mf.stream.pipe.stat;
+package org.culturegraph.mf.statistics;
 
 import static org.junit.Assert.assertEquals;
 
