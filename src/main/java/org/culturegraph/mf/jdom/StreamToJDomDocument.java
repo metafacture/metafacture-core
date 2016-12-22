a016b2390549c008d4b6a9120fb692d07bb0c9ac
Christoph Böhme <christoph@b3e.net>
2016-12-22 22:48:42 +0100

    Create jdom package
    The `jdom` package contains modules for working with JDOM documents.

--- src/main/java/org/culturegraph/mf/jdom/StreamToJDomDocument.java
+++ src/main/java/org/culturegraph/mf/jdom/StreamToJDomDocument.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.culturegraph.mf.stream.converter;
+package org.culturegraph.mf.jdom;
 
 import java.io.IOException;
 import java.util.HashMap;
