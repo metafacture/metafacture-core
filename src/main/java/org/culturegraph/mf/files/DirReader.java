c567ee845e5aedc8e78e22c09c2900a1b9598579
Christoph Böhme <christoph@b3e.net>
2016-12-22 22:26:01 +0100

    Create files package
    The `files` package contains modules that perform file operations.

--- src/main/java/org/culturegraph/mf/files/DirReader.java
+++ src/main/java/org/culturegraph/mf/files/DirReader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.culturegraph.mf.stream.source;
+package org.culturegraph.mf.files;
 
 import java.io.File;
 import java.io.FilenameFilter;
