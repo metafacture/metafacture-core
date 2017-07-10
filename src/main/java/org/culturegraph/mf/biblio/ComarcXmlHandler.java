4b49ba078e7bfa267c668bf6488824c79bfc60a6
Christoph Böhme <christoph@b3e.net>
2017-07-10 18:34:58 +0200

    Correct package of ComarcXmlHandler
    The package statement did not match the location of the class in the
    source tree.

--- src/main/java/org/culturegraph/mf/biblio/ComarcXmlHandler.java
+++ src/main/java/org/culturegraph/mf/biblio/ComarcXmlHandler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.culturegraph.mf.biblio.comarc;
+package org.culturegraph.mf.biblio;
 
 import java.util.ArrayList;
 import java.util.List;
