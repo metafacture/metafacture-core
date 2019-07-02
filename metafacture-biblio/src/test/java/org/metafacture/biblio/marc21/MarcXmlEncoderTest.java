d32fc00e58f2e7348b2cf4e7988d811617cc8191
Pascal Christoph <christoph@hbz-nrw.de>
2019-07-02 15:31:13 +0200

    Reformat ; use more constants; add header
    See #300.

--- metafacture-biblio/src/test/java/org/metafacture/biblio/marc21/MarcXmlEncoderTest.java
+++ metafacture-biblio/src/test/java/org/metafacture/biblio/marc21/MarcXmlEncoderTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Pascal Christoph (hbz)
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.metafacture.biblio.marc21;
 
 import static org.junit.Assert.assertEquals;
@@ -8,11 +24,29 @@ import org.junit.Before;
 import org.junit.Test;
 import org.metafacture.framework.helpers.DefaultObjectReceiver;
 
+/**
+ * Tests for class {@link MarcXmlEncoder}.
+ *
+ * @author some Jan (Eberhardt) did almost all
+ * @author Pascal Christoph (dr0i) dug it up again
+ *
+ */
+
 public class MarcXmlEncoderTest {
-
-    private StringBuilder resultCollector;
-    private MarcXmlEncoder encoder;
-
+    private static StringBuilder resultCollector ;
+    private static MarcXmlEncoder encoder ;
+    private static final String XML_DECLARATION = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+    private static final String XML_1_DECLARATION = "<?xml version=\"1.1\" encoding=\"UTF-8\"?>";
+    private static final String XML_16_DECLARATION = "<?xml version=\"1.0\" encoding=\"UTF-16\"?>";
+    private static final String XML_ROOT_OPEN = "<marc:collection xmlns:marc=\"http://www.loc.gov/MARC21/slim\" "
+            + "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/MARC21"
+            + "/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd\">";
+    private static final String XML_RECORD = "<marc:record><marc:controlfield tag=\"001\">92005291</marc:controlfield>"
+            + "<marc:datafield tag=\"010\" ind1=\" \" ind2=\" \"><marc:subfield code=\"a\">92005291</marc:subfield>"
+            + "</marc:datafield></marc:record>";
+    private static final String XML_MARC_COLLECTION_END_TAG = "</marc:collection>";
+    private static final String RECORD_ID = "92005291";
+    
     @Before
     public void setUp() throws Exception {
         encoder = new MarcXmlEncoder();
@@ -23,7 +57,7 @@ public class MarcXmlEncoderTest {
                 resultCollector.append(obj);
             }
         });
-        resultCollector = new StringBuilder();
+        resultCollector= new StringBuilder();
     }
 
     @After
@@ -31,10 +65,10 @@ public class MarcXmlEncoderTest {
     }
 
     private void addOneRecord(MarcXmlEncoder encoder) {
-        encoder.startRecord("92005291");
-        encoder.literal("001", "92005291");
+        encoder.startRecord(RECORD_ID);
+        encoder.literal("001", RECORD_ID);
         encoder.startEntity("010  ");
-        encoder.literal("a", "92005291");
+        encoder.literal("a", RECORD_ID);
         encoder.endEntity();
         encoder.endRecord();
     }
@@ -46,7 +80,7 @@ public class MarcXmlEncoderTest {
         encoder.closeStream();
 
         String actual = resultCollector.toString();
-        assertTrue(actual.startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"));
+        assertTrue(actual.startsWith(XML_DECLARATION));
     }
 
     @Test
@@ -56,7 +90,7 @@ public class MarcXmlEncoderTest {
         encoder.closeStream();
         String actual = resultCollector.toString();
         assertTrue(actual.startsWith("<marc:collection"));
-        assertTrue(actual.endsWith("</marc:collection>"));
+        assertTrue(actual.endsWith(XML_MARC_COLLECTION_END_TAG));
     }
 
     @Test
@@ -67,7 +101,7 @@ public class MarcXmlEncoderTest {
         encoder.closeStream();
 
         String actual = resultCollector.toString();
-        assertTrue(actual.startsWith("<?xml version=\"1.1\" encoding=\"UTF-8\"?>"));
+        assertTrue(actual.startsWith(XML_1_DECLARATION));
     }
 
     @Test
@@ -78,7 +112,7 @@ public class MarcXmlEncoderTest {
         encoder.closeStream();
 
         String actual = resultCollector.toString();
-        assertTrue(actual.startsWith("<?xml version=\"1.0\" encoding=\"UTF-16\"?>"));
+        assertTrue(actual.startsWith(XML_16_DECLARATION));
     }
 
     @Test
@@ -86,7 +120,8 @@ public class MarcXmlEncoderTest {
         encoder.startRecord("1");
         encoder.endRecord();
         encoder.closeStream();
-        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><marc:collection xmlns:marc=\"http://www.loc.gov/MARC21/slim\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd\"><marc:record></marc:record></marc:collection>";
+        String expected = XML_DECLARATION + XML_ROOT_OPEN + "<marc:record></marc:record>"
+               + XML_MARC_COLLECTION_END_TAG;
         String actual = resultCollector.toString();
         assertEquals(expected, actual);
     }
@@ -95,7 +130,7 @@ public class MarcXmlEncoderTest {
     public void createARecord() throws Exception {
         addOneRecord(encoder);
         encoder.closeStream();
-        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><marc:collection xmlns:marc=\"http://www.loc.gov/MARC21/slim\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd\"><marc:record><marc:controlfield tag=\"001\">92005291</marc:controlfield><marc:datafield tag=\"010\" ind1=\" \" ind2=\" \"><marc:subfield code=\"a\">92005291</marc:subfield></marc:datafield></marc:record></marc:collection>";
+        String expected = XML_DECLARATION + XML_ROOT_OPEN + XML_RECORD + "</marc:collection>";
         String actual = resultCollector.toString();
         assertEquals(expected, actual);
     }
@@ -105,10 +140,8 @@ public class MarcXmlEncoderTest {
         addOneRecord(encoder);
         addOneRecord(encoder);
         encoder.closeStream();
-
-        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><marc:collection xmlns:marc=\"http://www.loc.gov/MARC21/slim\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd\"><marc:record><marc:controlfield tag=\"001\">92005291</marc:controlfield><marc:datafield tag=\"010\" ind1=\" \" ind2=\" \"><marc:subfield code=\"a\">92005291</marc:subfield></marc:datafield></marc:record><marc:record><marc:controlfield tag=\"001\">92005291</marc:controlfield><marc:datafield tag=\"010\" ind1=\" \" ind2=\" \"><marc:subfield code=\"a\">92005291</marc:subfield></marc:datafield></marc:record></marc:collection>";
+        String expected = XML_DECLARATION + XML_ROOT_OPEN + XML_RECORD + XML_RECORD + "</marc:collection>";
         String actual = resultCollector.toString();
-
         assertEquals(expected, actual);
     }
 
@@ -120,7 +153,8 @@ public class MarcXmlEncoderTest {
         encoder.endEntity();
         encoder.endRecord();
         encoder.closeStream();
-        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><marc:collection xmlns:marc=\"http://www.loc.gov/MARC21/slim\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd\"><marc:record><marc:leader>dummy</marc:leader></marc:record></marc:collection>";
+        String expected = XML_DECLARATION + XML_ROOT_OPEN + "<marc:record><marc:leader>dummy</marc:leader></marc:record>"
+               + XML_MARC_COLLECTION_END_TAG;
         String actual = resultCollector.toString();
         assertEquals(expected, actual);
     }
