34472466255ee97ed71edd59284e95c14ed6f2ce
Fabian Steeg <steeg@hbz-nrw.de>
2020-02-06 16:25:00 +0100

    Add ScriptExtractor with extract-script Flux command
    See https://github.com/metafacture/metafacture-core/issues/312
    See https://github.com/hbz/oerindex/issues/3

---
+++ metafacture-html/src/test/java/org/metafacture/html/ScriptExtractorTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2020 Fabian Steeg, hbz
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.metafacture.html;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.io.StringReader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.metafacture.framework.ObjectReceiver;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * Tests for {@link ScriptExtractor}.
+ *
+ * @author Fabian Steeg
+ *
+ */
+public final class ScriptExtractorTest {
+
+    private static final StringReader IN = new StringReader("<html><script>{\"code\":\"yo\"}");
+    private static final String OUT = "{\"code\":\"yo\"}";
+
+    private ScriptExtractor scriptExtractor;
+
+    @Mock
+    private ObjectReceiver<String> receiver;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        scriptExtractor = new ScriptExtractor();
+        scriptExtractor.setReceiver(receiver);
+    }
+
+    @Test
+    public void testShouldProcessRecordsFollowedbySeparator() {
+        scriptExtractor.process(IN);
+        verify(receiver).process(OUT);
+        verifyNoMoreInteractions(receiver);
+    }
+
+    @After
+    public void cleanup() {
+        scriptExtractor.closeStream();
+    }
+}
