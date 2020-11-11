442f447605b5170eb62a27870b1446fffa39b7c4
Fabian Steeg <steeg@hbz-nrw.de>
2020-11-11 09:46:14 +0100

    Add extract-element flux command based on CSS-style selector
    See:
    
    https://github.com/metafacture/metafacture-core/issues/312
    https://gitlab.com/oersi/oersi-etl/-/issues/27
    https://jsoup.org/cookbook/extracting-data/selector-syntax

---
+++ metafacture-html/src/main/java/org/metafacture/html/ElementExtractor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 Fabian Steeg, hbz
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.metafacture.html;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.metafacture.framework.FluxCommand;
+import org.metafacture.framework.ObjectReceiver;
+import org.metafacture.framework.annotations.Description;
+import org.metafacture.framework.annotations.In;
+import org.metafacture.framework.annotations.Out;
+import org.metafacture.framework.helpers.DefaultObjectPipe;
+
+/**
+ * Extracts the the specified element from an HTML document
+ *
+ * @author Fabian Steeg
+ */
+@Description("Extracts the specified element from an HTML document")
+@In(Reader.class)
+@Out(String.class)
+@FluxCommand("extract-element")
+public class ElementExtractor extends DefaultObjectPipe<Reader, ObjectReceiver<String>> {
+    private String selector;
+
+    /**
+     * @param selector The CSS-style jsoup selector, see https://jsoup.org/cookbook/extracting-data/selector-syntax
+     */
+    public ElementExtractor(final String selector) {
+        this.selector = selector;
+    }
+
+    @Override
+    public void process(final Reader reader) {
+        try {
+            Document document = Jsoup.parse(IOUtils.toString(reader));
+            Element firstElement = document.select(selector).first();
+            getReceiver().process(firstElement.data());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
