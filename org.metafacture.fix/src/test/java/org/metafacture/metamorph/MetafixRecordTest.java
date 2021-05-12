d70d5d3b1405ab845c90f5ed9e4b76a52d73db02
Fabian Steeg <steeg@hbz-nrw.de>
2021-05-12 15:06:39 +0200

    Support `@SuppressWarnings` for Checkstyle, add tests (#35)
    Passing (nested fields) and disabled tests (e.g. else, see #10)

--- org.metafacture.fix/src/test/java/org/metafacture/metamorph/MetafixRecordTest.java
+++ org.metafacture.fix/src/test/java/org/metafacture/metamorph/MetafixRecordTest.java
@@ -18,6 +18,7 @@ package org.metafacture.metamorph;
 
 import org.metafacture.framework.StreamReceiver;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -36,6 +37,7 @@ import java.util.Arrays;
  * @author Fabian Steeg
  */
 @ExtendWith(MockitoExtension.class)
+@SuppressWarnings("checkstyle:MultipleStringLiterals")
 public class MetafixRecordTest {
 
     @RegisterExtension
@@ -48,20 +50,19 @@ public class MetafixRecordTest {
     }
 
     @Test
-    public void shouldSupportLookingAtOtherFieldsAny() {
-        final String name = "name";
+    public void ifAnyMatch() {
         MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(//
                 "if any_match('name', '.*University.*')", //
                 "  add_field('type', 'Organization')", //
                 "end"), //
             i -> {
                 i.startRecord("1");
-                i.literal(name, "Max Musterman");
+                i.literal("name", "Max Musterman");
                 i.endRecord();
                 //
                 i.startRecord("2");
-                i.literal(name, "Some University");
-                i.literal(name, "Filibandrina");
+                i.literal("name", "Some University");
+                i.literal("name", "Filibandrina");
                 i.endRecord();
             }, o -> {
                 o.get().startRecord("1");
@@ -74,21 +75,20 @@ public class MetafixRecordTest {
     }
 
     @Test
-    public void shouldSupportLookingAtOtherFieldsAll() {
-        final String name = "name";
+    public void ifAllMatch() {
         MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(//
                 "if all_match('name', '.*University.*')", //
                 "  add_field('type', 'Organization')", //
                 "end"), //
             i -> {
                 i.startRecord("1");
-                i.literal(name, "Max Musterman");
-                i.literal(name, "A University");
+                i.literal("name", "Max Musterman");
+                i.literal("name", "A University");
                 i.endRecord();
                 //
                 i.startRecord("2");
-                i.literal(name, "Some University");
-                i.literal(name, "University Filibandrina");
+                i.literal("name", "Some University");
+                i.literal("name", "University Filibandrina");
                 i.endRecord();
             }, o -> {
                 o.get().startRecord("1");
@@ -100,4 +100,128 @@ public class MetafixRecordTest {
             });
     }
 
+    @Test
+    public void ifAnyMatchNested() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(//
+                "if any_match('author.name.label', '.*University.*')", //
+                "  add_field('author.type', 'Organization')", //
+                "end"), //
+            i -> {
+                i.startRecord("1");
+                i.startEntity("author");
+                i.startEntity("name");
+                i.literal("label", "Max Musterman");
+                i.endEntity();
+                i.endEntity();
+                i.endRecord();
+                //
+                i.startRecord("2");
+                i.startEntity("author");
+                i.startEntity("name");
+                i.literal("label", "Some University");
+                i.endEntity();
+                i.endEntity();
+                i.endRecord();
+            }, o -> {
+                o.get().startRecord("1");
+                o.get().endRecord();
+                //
+                o.get().startRecord("2");
+                o.get().startEntity("author");
+                o.get().literal("type", "Organization");
+                o.get().endEntity();
+                o.get().endRecord();
+            });
+    }
+
+    @Test
+    @Disabled // TODO: fix events for this record order
+    public void ifAnyMatchFirst() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(//
+                "if any_match('name', '.*University.*')", //
+                "  add_field('type', 'Organization')", //
+                "end"), //
+            i -> {
+                i.startRecord("1");
+                i.literal("name", "Some University");
+                i.endRecord();
+                //
+                i.startRecord("2");
+                i.literal("name", "Max Musterman");
+                i.endRecord();
+            }, o -> {
+                o.get().startRecord("1");
+                o.get().literal("type", "Organization");
+                o.get().endRecord();
+                //
+                o.get().startRecord("2");
+                o.get().endRecord();
+            });
+    }
+
+    @Test
+    @Disabled // TODO: support else block
+    public void ifAnyMatchElse() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(//
+                "if any_match('name', '.*University.*')", //
+                "  add_field('type', 'Organization')", //
+                "else", //
+                "  add_field('type', 'Person')", //
+                "end"), //
+            i -> {
+                i.startRecord("1");
+                i.literal("name", "Max Musterman");
+                i.endRecord();
+                //
+                i.startRecord("2");
+                i.literal("name", "Some University");
+                i.endRecord();
+            }, o -> {
+                o.get().startRecord("1");
+                o.get().literal("type", "Person");
+                o.get().endRecord();
+                //
+                o.get().startRecord("2");
+                o.get().literal("type", "Organization");
+                o.get().endRecord();
+            });
+    }
+
+    @Test
+    @Disabled // TODO: support elsif block
+    public void ifAnyMatchElsif() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(//
+                "if any_match('name', '.*University.*')", //
+                "  add_field('type', 'Organization')", //
+                "elsif any_match('name', '[^ ]* [^ ]*')", //
+                "  add_field('type', 'Person')", //
+                "else", //
+                "  add_field('type', 'Unknown')", //
+                "end"), //
+            i -> {
+                i.startRecord("1");
+                i.literal("name", "Max Musterman");
+                i.endRecord();
+                //
+                i.startRecord("2");
+                i.literal("name", "Some University");
+                i.endRecord();
+                //
+                i.startRecord("3");
+                i.literal("name", "Filibandrina");
+                i.endRecord();
+            }, o -> {
+                o.get().startRecord("1");
+                o.get().literal("type", "Person");
+                o.get().endRecord();
+                //
+                o.get().startRecord("2");
+                o.get().literal("type", "Organization");
+                o.get().endRecord();
+                //
+                o.get().startRecord("3");
+                o.get().literal("type", "Unknown");
+                o.get().endRecord();
+            });
+    }
 }
