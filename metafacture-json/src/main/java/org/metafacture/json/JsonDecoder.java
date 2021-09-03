5ffe4b591132fb49732274ffffb5773a5022c8d8
Fabian Steeg <steeg@hbz-nrw.de>
2021-09-03 15:51:20 +0200

    Remove unused import (#382)

--- metafacture-json/src/main/java/org/metafacture/json/JsonDecoder.java
+++ metafacture-json/src/main/java/org/metafacture/json/JsonDecoder.java
@@ -33,7 +33,6 @@ import org.metafacture.framework.helpers.DefaultObjectPipe;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
