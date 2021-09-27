c42646f444c664fe83177ad0fc59419cdaa4aa34
Pascal Christoph <christoph@hbz-nrw.de>
2021-09-27 17:38:28 +0200

    Fix javadoc errors

--- metafacture-xml/src/main/java/org/metafacture/xml/XmlElementSplitter.java
+++ metafacture-xml/src/main/java/org/metafacture/xml/XmlElementSplitter.java
@@ -62,8 +62,8 @@ public final class XmlElementSplitter extends DefaultXmlPipe<StreamReceiver> {
     /**
      * Enriched constructor setting the top level element and the Element name
      *
-     * @param aTopLevelElement the name of the top level XML tag
-     * @param aElementName the name of the tag defining a new Element to be split
+     * @param topLevelElement the name of the top level XML tag
+     * @param elementName the name of the tag defining a new Element to be split
      */
     public XmlElementSplitter(final String topLevelElement, final String elementName) {
         setTopLevelElement(topLevelElement);
@@ -83,7 +83,7 @@ public final class XmlElementSplitter extends DefaultXmlPipe<StreamReceiver> {
     /**
      * Sets the top-level XML document element.
      *
-     * @param root the top level element. Don't set it to omit setting top level
+     * @param newRoot the top level element. Don't set it to omit setting top level
      *             element.
      */
     public void setTopLevelElement(final String newRoot) {
