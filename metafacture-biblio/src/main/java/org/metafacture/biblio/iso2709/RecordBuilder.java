metafacture/metafacture-core

commit 86bfdb4c6a400d430e3de94f065564f561022473

Add MissingJavadocMethod to checkstyle
Allow missing java doc for empty methods and constructors.

- fix javadoc
- simplify Marc21Encoder's non-arg constructor

See #396.

metafacture-biblio/src/main/java/org/metafacture/biblio/iso2709/RecordBuilder.java
@@ -56,6 +56,12 @@ public final class RecordBuilder {
     private AppendState appendState;
     private int fieldStart;
 
+    /**
+     * Initializes the RecordBuilder based on a RecordFormat.
+     *
+     * @param recordFormat RecordFormat as base to configure the RecordBuilder
+     * @see RecordFormat
+     */
     public RecordBuilder(final RecordFormat recordFormat) {
         Require.notNull(recordFormat);
         label = new LabelBuilder(recordFormat);
@@ -82,6 +88,13 @@ public final class RecordBuilder {
         return chars;
     }
 
+    /**
+     * Sets the charset of the FieldsBuilder.
+     *
+     * @param charset Charset used by the FieldsBuilder
+     * @see FieldsBuilder
+     * @see Charset
+     */
     public void setCharset(final Charset charset) {
         fields.setCharset(Require.notNull(charset));
     }
@@ -90,11 +103,25 @@ public final class RecordBuilder {
         return fields.getCharset();
     }
 
+    /**
+     * Sets the record status of the LabelBuilder.
+     *
+     * @param recordStatus 7 bit char record status
+     * @see Iso2709Constants.RECORD_STATUS_POS
+     * @see LabelBuilder
+     */
     public void setRecordStatus(final char recordStatus) {
         require7BitAscii(recordStatus);
         label.setRecordStatus(recordStatus);
     }
 
+    /**
+     * Sets the impl codes in the LabelBuilder.
+     *
+     * @param implCodes char array of 7 bit impl codes
+     * @see Iso2709Constants
+     * @see LabelBuilder
+     */
     public void setImplCodes(final char[] implCodes) {
         Require.notNull(implCodes);
         Require.that(implCodes.length == Iso2709Constants.IMPL_CODES_LENGTH);
@@ -102,12 +129,27 @@ public final class RecordBuilder {
         label.setImplCodes(implCodes);
     }
 
+    /**
+     * Sets an impl code at a given position in the LabelBuilder.
+     *
+     * @param index    index of the 7 bit impl code
+     * @param implCode char of a 7 bit impl code
+     * @see Iso2709Constants
+     * @see LabelBuilder
+     */
     public void setImplCode(final int index, final char implCode) {
         Require.that(0 <= index && index < Iso2709Constants.IMPL_CODES_LENGTH);
         require7BitAscii(implCode);
         label.setImplCode(index, implCode);
     }
 
+    /**
+     * Sets the system chars in the LabelBuilder.
+     *
+     * @param systemChars 7-bit char array to be set as system chars
+     * @see Iso2709Constants
+     * @see LabelBuilder
+     */
     public void setSystemChars(final char[] systemChars) {
         Require.notNull(systemChars);
         Require.that(systemChars.length == Iso2709Constants.SYSTEM_CHARS_LENGTH);
@@ -115,21 +157,47 @@ public final class RecordBuilder {
         label.setSystemChars(systemChars);
     }
 
+    /**
+     * Sets a system char at the given position in the LabelBuilder.
+     *
+     * @param systemChar 7-bit char to be set as the system char
+     * @param index      position of the system char
+     * @see Iso2709Constants
+     * @see LabelBuilder
+     */
     public void setSystemChar(final int index, final char systemChar) {
         Require.that(0 <= index && index < Iso2709Constants.SYSTEM_CHARS_LENGTH);
         require7BitAscii(systemChar);
         label.setSystemChar(index, systemChar);
     }
 
+    /**
+     * Sets a reserved char in the LabelBuilder.
+     *
+     * @param reservedChar 7-bit char to be set as reserved char
+     * @see Iso2709Constants
+     * @see LabelBuilder
+     */
     public void setReservedChar(final char reservedChar) {
         require7BitAscii(reservedChar);
         label.setReservedChar(reservedChar);
     }
 
+    /**
+     * Appends an identifier field.
+     *
+     * @param value String that is appended as an identfier field
+     */
     public void appendIdentifierField(final String value) {
         appendIdentifierField(defaultImplDefinedPart, value);
     }
 
+    /**
+     * Appends an identifier field in dependency of the current impl defined part.
+     *
+     * @param currentImplDefinedPart char array of the current impl defined part
+     * @param value                  String that is appended as an identfier field
+     */
     public void appendIdentifierField(final char[] currentImplDefinedPart, final String value) {
         requireNotInDataField();
         requireNotAppendingReferenceFields();
@@ -144,6 +212,14 @@ public final class RecordBuilder {
         appendReferenceField(currentTag, defaultImplDefinedPart, value);
     }
 
+    /**
+     * Appends a reference field in dependency of the current tag and of the
+     * current impl defined part.
+     *
+     * @param currentTag             char array of the current tag
+     * @param currentImplDefinedPart char array of the current impl defined part
+     * @param value                  String that is appended as a reference field
+     */
     public void appendReferenceField(final char[] currentTag, final char[] currentImplDefinedPart, final String value) {
         requireNotInDataField();
         requireNotAppendingDataFields();
@@ -181,6 +257,14 @@ public final class RecordBuilder {
         startDataField(currentTag, indicators, defaultImplDefinedPart);
     }
 
+    /**
+     * Starts a data field in dependency of the current tag, indicators and of the
+     * current impl defined part.
+     *
+     * @param currentTag             char array of the current tag
+     * @param indicators             char array of the current indicators
+     * @param currentImplDefinedPart char array of the current impl defined part
+     */
     public void startDataField(final char[] currentTag, final char[] indicators, final char[] currentImplDefinedPart) {
         requireNotInDataField();
         Require.notNull(currentTag);
@@ -208,6 +292,9 @@ public final class RecordBuilder {
         System.arraycopy(source, 0, destination, 0, destination.length);
     }
 
+    /**
+     * Ends a data field.
+     */
     public void endDataField() {
         requireInDataField();
         final int fieldEnd = fields.endField();
@@ -221,12 +308,23 @@ public final class RecordBuilder {
         }
     }
 
+    /**
+     * Appends a subfield.
+     *
+     * @param value String of the to be appended subfield
+     */
     public void appendSubfield(final String value) {
         requireInDataField();
         Require.notNull(value);
         fields.appendSubfield(defaultIdentifier, value);
     }
 
+    /**
+     * Appends a subfield in dependency of an identifier.
+     *
+     * @param identifier char array of an identifier
+     * @param value      String of the to be appended subfield
+     */
     public void appendSubfield(final char[] identifier, final String value) {
         requireInDataField();
         Require.notNull(identifier);
@@ -236,6 +334,11 @@ public final class RecordBuilder {
         fields.appendSubfield(identifier, value);
     }
 
+    /**
+     * Builds the record.
+     *
+     * @return byte array of the record
+     */
     public byte[] build() {
         requireNotInDataField();
         final int baseAddress = Iso2709Constants.RECORD_LABEL_LENGTH + directory.length();
@@ -286,6 +389,12 @@ public final class RecordBuilder {
         Require.that(charCode != Iso646Constants.INFORMATION_SEPARATOR_3);
     }
 
+    /**
+     * Resets the label, directory and the fields. Sets the "append state" to "id
+     * field".
+     *
+     * @see AppendState.ID_FIELD
+     */
     public void reset() {
         label.reset();
         directory.reset();

config/checkstyle/checkstyle.xml
@@ -82,6 +82,9 @@
         <module name="MethodTypeParameterName"/>
         <module name="MissingCtor"/>
         <module name="MissingDeprecated"/>
+        <module name="MissingJavadocMethod">
+            <property name="minLineCount" value="1"/>
+        </module>
         <module name="MissingOverride"/>
         <module name="MissingSwitchDefault"/>
         <module name="ModifiedControlVariable"/>

metafacture-biblio/src/main/java/org/metafacture/biblio/iso2709/RecordFormat.java
@@ -51,6 +51,15 @@ public final class RecordFormat {
     private final int fieldStartLength;
     private final int implDefinedPartLength;
 
+    /**
+     * Initializes the RecordFormat.
+     *
+     * @param indicatorLength       the length of the indicator
+     * @param identifierLength      the length of the identifier
+     * @param fieldLengthLength     the length of the field
+     * @param fieldStartLength      the length of the indicator
+     * @param implDefinedPartLength the length of the part
+     */
     public RecordFormat(final int indicatorLength, final int identifierLength,
             final int fieldLengthLength, final int fieldStartLength,
             final int implDefinedPartLength) {
@@ -61,6 +70,11 @@ public final class RecordFormat {
         this.implDefinedPartLength = implDefinedPartLength;
     }
 
+    /**
+     * Initializes a RecordFormat defined by a RecordFormat.
+     *
+     * @param source the RecordFormat
+     */
     public RecordFormat(final RecordFormat source) {
         Require.notNull(source);
 
@@ -71,10 +85,21 @@ public final class RecordFormat {
         implDefinedPartLength = source.implDefinedPartLength;
     }
 
+    /**
+     * Returns a new default Builder.
+     *
+     * @return Builder
+     */
     public static Builder create() {
         return new Builder();
     }
 
+    /**
+     * Returns a new Builder created from a RecordFormat.
+     *
+     * @param source the RecordFormat
+     * @return Builder
+     */
     public static Builder createFrom(final RecordFormat source) {
         return create()
                 .withIndicatorLength(source.indicatorLength)
@@ -149,6 +174,12 @@ public final class RecordFormat {
         Builder() {
         }
 
+        /**
+         * Returns a new Builder with defined indicator length.
+         *
+         * @param currentIndicatorLength the length of the indicator
+         * @return Builder
+         */
         public Builder withIndicatorLength(final int currentIndicatorLength) {
             Require.notNegative(currentIndicatorLength);
             Require.that(currentIndicatorLength < RADIX);
@@ -156,6 +187,12 @@ public final class RecordFormat {
             return this;
         }
 
+        /**
+         * Returns a new Builder with defined identifier length.
+         *
+         * @param currentIdentifierLength the length of the identifier
+         * @return Builder
+         */
         public Builder withIdentifierLength(final int currentIdentifierLength) {
             Require.notNegative(currentIdentifierLength);
             Require.that(currentIdentifierLength < RADIX);
@@ -163,6 +200,12 @@ public final class RecordFormat {
             return this;
         }
 
+        /**
+         * Returns a new Builder with defined field length.
+         *
+         * @param currentFieldLengthLength the length of the field
+         * @return Builder
+         */
         public Builder withFieldLengthLength(final int currentFieldLengthLength) {
             Require.that(currentFieldLengthLength > 0);
             Require.that(currentFieldLengthLength < RADIX);
@@ -170,6 +213,12 @@ public final class RecordFormat {
             return this;
         }
 
+        /**
+         * Returns a new Builder with defined field start length.
+         *
+         * @param currentFieldStartLength the length of the field start
+         * @return Builder
+         */
         public Builder withFieldStartLength(final int currentFieldStartLength) {
             Require.that(currentFieldStartLength > 0);
             Require.that(currentFieldStartLength < RADIX);
@@ -177,6 +226,12 @@ public final class RecordFormat {
             return this;
         }
 
+        /**
+         * Returns a new Builder with defined part length.
+         *
+         * @param currentImplDefinedPartLength the length of the part
+         * @return Builder
+         */
         public Builder withImplDefinedPartLength(final int currentImplDefinedPartLength) {
             Require.notNegative(currentImplDefinedPartLength);
             Require.that(currentImplDefinedPartLength < RADIX);
@@ -184,6 +239,11 @@ public final class RecordFormat {
             return this;
         }
 
+        /**
+         * Returns a new RecordFormat.
+         *
+         * @return RecordFormat
+         */
         public RecordFormat build() {
             return new RecordFormat(indicatorLength, identifierLength,
                     fieldLengthLength, fieldStartLength, implDefinedPartLength);

metafacture-biblio/src/main/java/org/metafacture/biblio/marc21/Marc21Encoder.java
@@ -80,6 +80,10 @@ public final class Marc21Encoder extends
 
     private boolean generateIdField;
 
+    /**
+     * Initializes the encoder with MARC 21 constants and charset.
+     *
+     */
     public Marc21Encoder() {
         builder = new RecordBuilder(Marc21Constants.MARC21_FORMAT);
         builder.setCharset(Marc21Constants.MARC21_CHARSET);

metafacture-biblio/src/main/java/org/metafacture/biblio/marc21/MarcXmlEncoder.java
@@ -71,30 +71,20 @@ public final class MarcXmlEncoder extends DefaultStreamPipe<ObjectReceiver<Strin
     private static final int TAG_BEGIN = 0;
     private static final int TAG_END = 3;
 
-    private final StringBuilder builder;
+    private final StringBuilder builder = new StringBuilder();
 
-    private boolean atStreamStart;
+    private boolean atStreamStart = true;
 
     private boolean omitXmlDeclaration;
-    private String xmlVersion;
-    private String xmlEncoding;
+    private String xmlVersion = "1.0";
+    private String xmlEncoding = "UTF-8";
 
-    private String currentEntity;
+    private String currentEntity = "";
+
     private int indentationLevel;
-    private boolean formatted;
+    private boolean formatted = true;
 
     public MarcXmlEncoder() {
-        this.builder = new StringBuilder();
-        this.atStreamStart = true;
-
-        this.omitXmlDeclaration = false;
-        this.xmlVersion = "1.0";
-        this.xmlEncoding = "UTF-8";
-
-        this.currentEntity = "";
-
-        this.indentationLevel = 0;
-        this.formatted = true;
     }
 
     public void omitXmlDeclaration(final boolean currentOmitXmlDeclaration) {

commit 6c9bf162991ea701109884a1636094e31aeb91d1

Fix javadoc error introduced with 86bfdb4c6a400d430e3de94f065564f561022473.

metafacture-biblio/src/main/java/org/metafacture/biblio/iso2709/RecordBuilder.java
@@ -107,7 +107,7 @@ public final class RecordBuilder {
      * Sets the record status of the LabelBuilder.
      *
      * @param recordStatus 7 bit char record status
-     * @see Iso2709Constants.RECORD_STATUS_POS
+     * @see Iso2709Constants#RECORD_STATUS_POS
      * @see LabelBuilder
      */
     public void setRecordStatus(final char recordStatus) {
@@ -393,7 +393,7 @@ public final class RecordBuilder {
      * Resets the label, directory and the fields. Sets the "append state" to "id
      * field".
      *
-     * @see AppendState.ID_FIELD
+     * @see AppendState#ID_FIELD
      */
     public void reset() {
         label.reset();