c093da1cc27ab5b7dfd14a6c3fb84ff8af95bd4d
Pascal Christoph <christoph@hbz-nrw.de>
2023-06-12 15:56:18 +0200

    Switch to PercentEscaper (metafacture-fix#273)
    This enables both:
    whitespace converted to a plus sign "+" (which is the default to be backwards
    compatible) and whitespace escaped as percent "%20".
    
    - add guava dependency (uses the same version as metafacture-fix)
    - introduce test
    - add to schemata

---
+++ metamorph/src/test/java/org/metafacture/metamorph/functions/URLEncodeTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2023 hbz
+ *
+ * Licensed under the Apache License, Version 2.0 the "License";
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.metafacture.metamorph.functions;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * tests {@link ISBN}
+ *
+ * @author Pascal Christoph (dr0i)
+ */
+
+public final class URLEncodeTest {
+
+    private static final String CAFE_UTF8 = "café";
+    private static final String CAFE_ENCODED = "caf%C3%A9";
+    private static final String SOME_CHARS = "/&%\\+";
+    private static final String SOME_CHARS_ENCODED = "%2F%26%25%5C%2B";
+    private static final String WHITESPACE = " ";
+    private static final String WHITESPACE_AS_PLUS_ENCODED = "+";
+    private static final String WHITESPACE_PERCENT_ENCODED = "%20";
+
+    @Test
+    public void testUtf8(){
+        final URLEncode urlEncode = new URLEncode();
+        assertEquals(CAFE_ENCODED, urlEncode.process(CAFE_UTF8));
+    }
+    @Test
+    public void testSomeChars(){
+        final URLEncode urlEncode = new URLEncode();
+        assertEquals(SOME_CHARS_ENCODED, urlEncode.process(SOME_CHARS));
+    }
+    @Test
+    public void testEscapeSpaceAsPlus(){
+        final URLEncode urlEncode = new URLEncode();
+        assertEquals(WHITESPACE_AS_PLUS_ENCODED, urlEncode.process(WHITESPACE));
+    }
+
+    @Test
+    public void testEscapeSpaceAsPercentEncoded(){
+        final URLEncode urlEncode = new URLEncode();
+        urlEncode.setPlusForSpace(false);
+        assertEquals(WHITESPACE_PERCENT_ENCODED, urlEncode.process(WHITESPACE));
+    }
+
+    @Test
+    public void testSafeChars(){
+        final URLEncode urlEncode = new URLEncode();
+        urlEncode.setSafeChars(SOME_CHARS);
+        assertEquals(SOME_CHARS, urlEncode.process(SOME_CHARS));
+    }
+
+}
