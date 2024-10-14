ec7f9ec6f2c5bef938a70a3137642e6fbdd2e250
TobiasNx <61879957+TobiasNx@users.noreply.github.com>
2024-10-14 14:32:51 +0200

    Apply suggestions from code review
    Review by @fsteeg #370
    
    Co-authored-by: Fabian Steeg <steeg@hbz-nrw.de>

--- metafix/src/main/java/org/metafacture/metafix/Metafix.java
+++ metafix/src/main/java/org/metafacture/metafix/Metafix.java
@@ -61,11 +61,10 @@ import java.util.function.BiConsumer;
  * @author Christoph Böhme (Metamorph)
  * @author Fabian Steeg (Metafix)
  */
-@Description("Applies a fix transformation to the event stream. Fix definition is given in brackets.") // checkstyle-disable-line ClassDataAbstractionCoupling|ClassFanOutComplexity
+@Description("Applies a fix transformation to the event stream, given as the path to a fix file or the fixes themselves.") // checkstyle-disable-line ClassDataAbstractionCoupling|ClassFanOutComplexity
 @In(StreamReceiver.class)
 @Out(StreamReceiver.class)
 @FluxCommand("fix")
-
 public class Metafix implements StreamPipe<StreamReceiver>, Maps { // checkstyle-disable-line ClassDataAbstractionCoupling
     public static final String ARRAY_MARKER = "[]";
     public static final String FIX_EXTENSION = ".fix";
