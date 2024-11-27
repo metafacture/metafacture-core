03bc0099d7172cb539027c0bd62b6e27aa8c2dd2
Fabian Steeg <steeg@hbz-nrw.de>
2024-11-27 16:39:48 +0100

    Tweak test to fix compiler error and warnings, wait 100 ms (#495)

--- metafacture-flowcontrol/src/test/java/org/metafacture/flowcontrol/ObjectSleeperTest.java
+++ metafacture-flowcontrol/src/test/java/org/metafacture/flowcontrol/ObjectSleeperTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Christoph Böhme
+ * Copyright 2024 Tobias Bülte, hbz
  *
  * Licensed under the Apache License, Version 2.0 the "License";
  * you may not use this file except in compliance with the License.
@@ -17,15 +17,14 @@
 package org.metafacture.flowcontrol;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doThrow;
+
+import org.metafacture.framework.ObjectReceiver;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.metafacture.framework.MetafactureException;
-import org.metafacture.framework.ObjectReceiver;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
 import java.time.Duration;
 import java.time.Instant;
 
@@ -38,7 +37,7 @@ import java.time.Instant;
 public final class ObjectSleeperTest {
 
     @Mock
-    private ObjectReceiver<String> sleepTimer;
+    private ObjectReceiver<String> receiver;
 
     @Before
     public void setup() {
@@ -47,18 +46,18 @@ public final class ObjectSleeperTest {
 
     @Test
     public void shouldTestIfClockedTimeExceedsDuration() {
-    long sleepTime = 10;
+    long sleepTime = 100;
 
-    ObjectSleeper objectSleeper = new ObjectSleeper();
+    ObjectSleeper<String> objectSleeper = new ObjectSleeper<>();
+    objectSleeper.setReceiver(receiver);
     objectSleeper.setSleepTime(sleepTime);
     Instant start = Instant.now();
-    sleepTimer.objectSleeper;
+    objectSleeper.process(null);
     Instant end = Instant.now();
-
 
     Duration timeElapsed = Duration.between(start, end);
 
-    assertTrue(timeElapsed.toSeconds() >= sleepTime);
+    assertTrue(timeElapsed.toMillis() >= sleepTime);
 
     }
 
