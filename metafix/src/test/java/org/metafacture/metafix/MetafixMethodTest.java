ae889ad0fa254d49e3aee3aeaf68778e30b75ce2
TobiasNx <tobias.buelte@hbz-nrw.de>
2026-03-25 10:23:23 +0100

    Add Minus Function #644

--- metafix/src/test/java/org/metafacture/metafix/MetafixMethodTest.java
+++ metafix/src/test/java/org/metafacture/metafix/MetafixMethodTest.java
@@ -4286,7 +4286,6 @@ public class MetafixMethodTest {
         );
     }
 
-
     @Test
     public void shouldPlusValue() {
         MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
@@ -4524,6 +4523,261 @@ public class MetafixMethodTest {
         MetafixTestHelpers.assertExecutionException(IllegalStateException.class, "Expected String, got Array", () ->
             MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
                     "plus('some.number?[]', '3')"
+                ),
+                i -> {
+                    i.startRecord("1");
+                    i.startEntity("some");
+                    i.startEntity("numbers[]");
+                    i.literal("1", "1");
+                    i.literal("2", "2");
+                    i.literal("3", "3");
+                    i.endEntity();
+                    i.endEntity();
+                    i.endRecord();
+                },
+                o -> {
+                }
+            )
+        );
+    }
+
+    @Test
+    public void shouldMinusValue() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
+                "minus(number, '3')"
+            ),
+            i -> {
+                i.startRecord("1");
+                i.literal("number", "1");
+                i.endRecord();
+            },
+            o -> {
+                o.get().startRecord("1");
+                o.get().literal("number", "-2");
+                o.get().endRecord();
+            }
+        );
+    }
+
+    @Test
+    public void shouldMinusValueInHash() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
+                "minus(number.name, '4')"
+            ),
+            i -> {
+                i.startRecord("1");
+                i.startEntity("number");
+                i.literal("name", "3");
+                i.literal("type", "TEST");
+                i.endEntity();
+                i.endRecord();
+            },
+            o -> {
+                o.get().startRecord("1");
+                o.get().startEntity("number");
+                o.get().literal("name", "-1");
+                o.get().literal("type", "TEST");
+                o.get().endEntity();
+                o.get().endRecord();
+            }
+        );
+    }
+
+    @Test
+    public void shouldMinusValueInArray() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
+                "minus('numbers[].1', '5')"
+            ),
+            i -> {
+                i.startRecord("1");
+                i.startEntity("animals[]");
+                i.literal("1", "2");
+                i.literal("2", "4");
+                i.literal("3", "6");
+                i.endEntity();
+                i.endRecord();
+            },
+            o -> {
+                o.get().startRecord("1");
+                o.get().startEntity("animals[]");
+                o.get().literal("1", "-2");
+                o.get().literal("2", "4");
+                o.get().literal("3", "6");
+                o.get().endEntity();
+                o.get().endRecord();
+            }
+        );
+    }
+
+    @Test
+    // See issue metafacture-fix#100
+    public void shouldMinusValueInEntireArray() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
+                "minus('numbers[].*', '6')"
+            ),
+            i -> {
+                i.startRecord("1");
+                i.startEntity("animals[]");
+                i.literal("1", "1");
+                i.literal("2", "2");
+                i.literal("3", "3");
+                i.endEntity();
+                i.endRecord();
+            },
+            o -> {
+                o.get().startRecord("1");
+                o.get().startEntity("animals[]");
+                o.get().literal("1", "-5");
+                o.get().literal("2", "-4");
+                o.get().literal("3", "-3");
+                o.get().endEntity();
+                o.get().endRecord();
+            }
+        );
+    }
+
+    @Test
+    // See issue #601
+    public void shouldMinusValueInNestedArray() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
+                "minus('nestedTest[].*.numbers[].*', '7')"
+            ),
+            i -> {
+                i.startRecord("1");
+                i.startEntity("nestedTest[]");
+                i.startEntity("1");
+                i.startEntity("numbers[]");
+                i.literal("1", "5");
+                i.literal("2", "10");
+                i.literal("3", "15");
+                i.endEntity();
+                i.endEntity();
+                i.startEntity("2");
+                i.startEntity("numbers[]");
+                i.literal("1", "20");
+                i.literal("2", "25");
+                i.literal("3", "30");
+                i.endEntity();
+                i.endEntity();
+                i.endEntity();
+                i.endRecord();
+            },
+            (o, f) -> {
+                o.get().startRecord("1");
+                o.get().startEntity("nestedTest[]");
+                o.get().startEntity("1");
+                o.get().startEntity("numbers[]");
+                o.get().literal("1", "-2");
+                o.get().literal("2", "3");
+                o.get().literal("3", "8");
+                f.apply(2).endEntity();
+                o.get().startEntity("2");
+                o.get().startEntity("numbers[]");
+                o.get().literal("1", "13");
+                o.get().literal("2", "18");
+                o.get().literal("3", "23");
+                f.apply(3).endEntity();
+                o.get().endRecord();
+            }
+        );
+    }
+
+    @Test
+    // See issue #601
+    public void shouldMinusValueInArraySubField() {
+        MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
+                "minus('coll[].*.number', '9')"
+            ),
+            i -> {
+                i.startRecord("1");
+                i.startEntity("coll[]");
+                i.startEntity("1");
+                i.literal("number", "1");
+                i.literal("b", "Dog");
+                i.endEntity();
+                i.startEntity("2");
+                i.literal("number", "4");
+                i.literal("b", "Ape");
+                i.endEntity();
+                i.startEntity("3");
+                i.literal("number", "7");
+                i.endEntity();
+                i.startEntity("4");
+                i.literal("number", "14");
+                i.endEntity();
+                i.endEntity();
+                i.endRecord();
+            },
+            (o, f) -> {
+                o.get().startRecord("1");
+                o.get().startEntity("coll[]");
+                o.get().startEntity("1");
+                o.get().literal("number", "-8");
+                o.get().literal("b", "Dog");
+                o.get().endEntity();
+                o.get().startEntity("2");
+                o.get().literal("number", "-5");
+                o.get().literal("b", "Ape");
+                o.get().endEntity();
+                o.get().startEntity("3");
+                o.get().literal("number", "-2");
+                o.get().endEntity();
+                o.get().startEntity("4");
+                o.get().literal("number", "5");
+                f.apply(2).endEntity();
+                o.get().endRecord();
+            }
+        );
+    }
+
+    @Test
+    // See issue metafacture-fix#100
+    public void shouldNotMinusValueToArray() {
+        MetafixTestHelpers.assertExecutionException(IllegalStateException.class, "Expected String, got Array", () ->
+            MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
+                    "minus('numbers[]', '3')"
+                ),
+                i -> {
+                    i.startRecord("1");
+                    i.startEntity("numbers[]");
+                    i.literal("1", "1");
+                    i.literal("2", "2");
+                    i.literal("3", "3");
+                    i.endEntity();
+                    i.endRecord();
+                },
+                o -> {
+                }
+            )
+        );
+    }
+
+    @Test
+    public void shouldNotMinusValueToArrayWithWildcard() {
+        MetafixTestHelpers.assertExecutionException(IllegalStateException.class, "Expected String, got Array", () ->
+            MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
+                    "minus('animal?[]', '4')"
+                ),
+                i -> {
+                    i.startRecord("1");
+                    i.startEntity("numbers[]");
+                    i.literal("1", "1");
+                    i.literal("2", "2");
+                    i.literal("3", "3");
+                    i.endEntity();
+                    i.endRecord();
+                },
+                o -> {
+                }
+            )
+        );
+    }
+
+    @Test
+    public void shouldNotMinusValueToArrayWithWildcardNested() {
+        MetafixTestHelpers.assertExecutionException(IllegalStateException.class, "Expected String, got Array", () ->
+            MetafixTestHelpers.assertFix(streamReceiver, Arrays.asList(
+                    "minus('some.number?[]', '3')"
                 ),
                 i -> {
                     i.startRecord("1");
